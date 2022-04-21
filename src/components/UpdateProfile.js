7490f993dd3889d47bf2677ffa01ae4e87c3893c
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-04-21 15:27:27 -0500

    Changed references to bio to profile
    Added a comment to the top explaining the component's functionality and primary contact.

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -1,28 +1,34 @@
 import { useState } from "react";
 
+/**
+ * A component that updates the user's profile (about me/bio). 
+ * Primary Contact: ASHETON
+ * @param {*} props 
+ * @returns 
+ */
 const UpdateProfile = props => {
 
     const API_URL = "http://localhost:8000";
     const FAKE_CURRENTUSER_ID = 1;
 
-    const [bioText, setBioText] = useState("");
+    const [profileText, setprofileText] = useState("");
 
     /**
-     * Change bioText on keystroke
+     * Change profileText on keystroke
      * @param {*} e 
      */
     function handleChange(e) {
-        setBioText(e.target.value);
+        setprofileText(e.target.value);
     }
 
     /**
      * Submit the updated bio (about me) information
      */
-    async function submitBio() {
-        console.log(bioText);
+    async function submitUpdatedProfile() {
+        console.log(profileText);
 
         const newProfile = {
-            "profile": bioText
+            "profile": profileText
         }
 
         //  Replace with props.currentUser in production
@@ -31,15 +37,16 @@ const UpdateProfile = props => {
                 method: 'PUT',
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(newProfile)
-            }).then(response => console.log(response.json()));
+            })
+            
     }
 
 
     return (
         <div className="update-profile-area">
-            <label htmlFor="bioText">About Me (150 characters max):</label><br></br>
-            <textarea type="text" id="bioText" name="bioText" minLength="0" maxLength="150" rows="5" cols="50" onChange={handleChange}></textarea><br></br>
-            <button value="submit" id="submitButton" onClick={submitBio}>Submit</button>
+            <label htmlFor="profileText">About Me (150 characters max):</label><br></br>
+            <textarea type="text" id="profileText" name="profileText" minLength="0" maxLength="150" rows="5" cols="50" onChange={handleChange}></textarea><br></br>
+            <button value="submit" id="submitButton" onClick={submitUpdatedProfile}>Submit</button>
         </div>
     );
 }
