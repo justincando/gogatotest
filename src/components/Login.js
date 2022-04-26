f73e15c915e8993c682d9902889edda296efe28f
JasonKovale <99827773+JasonKovale@users.noreply.github.com>
2022-04-26 17:26:52 -0400

    added login component

---
+++ src/components/Login.js
@@ -0,0 +1,84 @@
+// Author: Tyler, Jason, Boualem, Marcus
+import { useState, useEffect } from "react";
+
+// Functional component for User Registration
+function Login() {
+  //const [userToRegister, setUserToRegister] = useState(null);
+
+  // Contains fields for User input
+  const [inputFields, setInputFields] = useState({
+    username: "",
+    password: ""
+  });
+
+  //useEffect(() => console.log(userToRegister), [userToRegister]);
+  useEffect(() => console.log(inputFields), [inputFields]);
+
+  // Handles input event for User fields/registration
+  function getInput(event) {
+    setInputFields({
+      ...inputFields,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  //insert fetch call
+  async function loginUser(event) {
+    // Stop default form submission event
+    event.preventDefault();
+
+    // Register user
+    let userInfo = {
+      username: inputFields.username,
+      password: inputFields.password
+    };
+
+    // Submit info to server
+    await fetch("http://localhost:8000/login", {
+      headers: {
+        "content-type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(userInfo),
+    })
+      .then((response) => response.text())
+      .then(data => console.log(data));
+
+    // Decide on redirection here =====
+  }
+
+  return (
+    <>
+      <h1>Login</h1>
+
+      <form onSubmit={loginUser}>
+        {/* Username Field */}
+        Username:{" "}
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={inputFields.username}
+          onChange={getInput}
+          required
+        />
+        <br />
+        {/* Password Field */}
+        Password:{" "}
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={inputFields.password}
+          onChange={getInput}
+          required
+        />
+        <br />
+        
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import App from './App';
-import Registration from './components/Registration';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./css/index.css";
+import App from "./App";
+import Registration from "./components/Registration";
+import Login from "./components/Login";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Registration />
+    <Login />
   </React.StrictMode>
 );
-
