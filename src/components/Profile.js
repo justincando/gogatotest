8863bb98c25abd1f67a83de1ae75565c72f1a1ad
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-04-27 11:57:26 -0500

    Added test function and variables

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import UpdateAboutMe from "./UpdateAboutMe";
 
 function Profile(props) {
+    const FAKE_CURRENTUSER_ID = 1;
+    const FAKE_CURRENTUSER_UN = "Sonic";
+
+    useEffect(() => getProfileInfo());
+
+
+    function getProfileInfo() {
+        console.log("I am getProfileInfo");
+    }
 
     return (
-        <UpdateAboutMe/>
+        <>
+            <h1>{FAKE_CURRENTUSER_UN}'s profile </h1>
+            <UpdateAboutMe FAKE_CURRENTUSER_ID={FAKE_CURRENTUSER_ID} />
+        </>
     );
 
 }
