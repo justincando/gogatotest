005920827aee8617d6208ab1c4d89102fe893991
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-04-27 14:34:09 -0500

    Added ternaries, filled fetch info

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,21 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import UpdateAboutMe from "./UpdateAboutMe";
 
 function Profile(props) {
     const FAKE_CURRENTUSER_ID = 1;
-    const FAKE_CURRENTUSER_UN = "Sonic";
-
-    useEffect(() => getProfileInfo());
-
+    const API_URL = "http://localhost:8000";
+    const [data, setData] = useState();
 
     function getProfileInfo() {
-        console.log("I am getProfileInfo");
+        fetch(`${API_URL}/users/${FAKE_CURRENTUSER_ID}`)
+        .then((response) => response.json())
+        .then((response) => {
+            setData(response);
+            console.log(response)
+        })
+        .catch((error) => console.log(error))
     }
 
     return (
         <>
-            <h1>{FAKE_CURRENTUSER_UN}'s profile </h1>
-            <UpdateAboutMe FAKE_CURRENTUSER_ID={FAKE_CURRENTUSER_ID} />
+        {data == null ? getProfileInfo() : <h1>{data.username}'s profile </h1>}
+
+        {data != null && data.id == FAKE_CURRENTUSER_ID ? <UpdateAboutMe FAKE_CURRENTUSER_ID={FAKE_CURRENTUSER_ID} /> : null
+        }
+
+        <a href={`/person/${FAKE_CURRENTUSER_ID}`}>Test profile 1</a>
+        <a href={`/person/${2}`}>Test profile 2</a>
         </>
     );
 
