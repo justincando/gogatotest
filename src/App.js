ac948256dfd7a05fcf3e89094f72683beb67da43
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-04-27 15:54:14 -0500

    Reformatted, added route for dynamic profiles
    currentUser currently has a test value. In practice this value would be retrieved from localstorage and would be the userID.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import './css/fonts.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Registration from './components/Registration';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 function App() {
   return (
     <BrowserRouter>
-     <Navbar/> 
-    <Routes>
-    
-    <Route  path="/registration" element ={< Registration/>} /> 
-    <Route  path="/login" element ={< Login/>} /> 
+      <Navbar />
+      <Routes>
 
-    </Routes>
+        <Route path="/registration" element={< Registration />} />
+        <Route path="/login" element={< Login />} />
+        <Route exact path="/profile/:id" element={<Profile currentUser={1} />} />
+
+      </Routes>
     </BrowserRouter>
-
-
-
   );
 }
 
