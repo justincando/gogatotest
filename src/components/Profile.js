cb9f70068d6eb7510349c92d81bbbe6cc1db4522
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-04-28 11:53:17 -0500

    Brought fetch requests down to 2 from 4

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,6 +10,7 @@ function Profile(props) {
     let { id } = useParams();
     const API_URL = "http://localhost:8000";
     const [data, setData] = useState();
+    const [doNotUpdate, toggleDoNotUpdate] = useState(false);
     const [showHideEdit, toggleEditDisplay] = useState(false);
 
     /**
@@ -17,46 +18,57 @@ function Profile(props) {
      * @author Asheton
      */
     function getProfileInfo() {
-        fetch(`${API_URL}/users/${id}`)
-            .then((response) => response.json())
-            .then((response) => {
-                setData(response);
-                console.log(response)
-            })
-            .catch((error) => console.log(error))
+        if (!doNotUpdate) {
+            toggleDoNotUpdate(true);
+            fetch(`${API_URL}/users/${id}`)
+                .then((response) => response.json())
+                .then((response) => {
+                    setData(response);
+                    //console.log(response)
+                })
+                .catch((error) => console.log(error))
+        }
     }
 
     function showHide() {
         toggleEditDisplay(!showHideEdit);
     }
 
+
+    useEffect(() => {
+        if (!doNotUpdate) {
+            getProfileInfo();
+            //console.log('data has been fetched');
+        }
+    }, []);
+
     return (
         <>
-        <div id="profile-page">
+            <div id="profile-page">
 
-            {data == null ? getProfileInfo() :
-                <div id="profile-data">
+                {data != null ?
+                    <div id="profile-data">
 
-                    <h1>{data.username}'s profile </h1>
-                    <h3>About Me: {data.aboutMe}</h3>
-                    <h3>Followers: {data.followerCount}</h3>
-                    <h3>Points: {data.points}</h3>
-                    <h3>First Name: {data.firstName}</h3>
-                    <h3>Last Name: {data.lastName}</h3>
-                    <h3>Email: {data.email}</h3>
+                        <h1>{data.username}'s profile </h1>
+                        <h3>About Me: {data.aboutMe}</h3>
+                        <h3>Followers: {data.followerCount}</h3>
+                        <h3>Points: {data.points}</h3>
+                        <h3>First Name: {data.firstName}</h3>
+                        <h3>Last Name: {data.lastName}</h3>
+                        <h3>Email: {data.email}</h3>
 
-                </div>}
+                    </div> : null}
 
-            {data != null && data.id == props.currentUser ? 
-            <div id="profile-edit-options">
-                <button onClick={showHide}>Edit</button>
-                {showHideEdit && <UpdateAboutMe id={id} />}
+                {data != null && data.id == props.currentUser ?
+                    <div id="profile-edit-options">
+                        <button onClick={showHide}>Edit</button>
+                        {showHideEdit && <UpdateAboutMe id={id} />}
+                    </div>
+                    : null}
+
+                <a href={`/profile/${1}`}>Test profile 1</a><br></br>
+                <a href={`/profile/${2}`}>Test profile 2</a>
             </div>
-             : null}
-
-            <a href={`/profile/${1}`}>Test profile 1</a><br></br>
-            <a href={`/profile/${2}`}>Test profile 2</a>
-        </div>
         </>
     );
 
