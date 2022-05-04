0ba5e7c3dc5d9e4033c42943261f3523ee4e712d
Marcus Glover <marcusg73@yahoo.com>
2022-05-04 12:46:16 -0400

    Changed button name

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -38,7 +38,7 @@ const CreatePost = (props) => {
          <form onSubmit={handleSubmit} className="center">
             <p><label htmlFor="post input"><h3>What would you like to share? 📝</h3></label></p>
             <p><input className="center" type="textarea" name={postContent} onChange={handleChange} autoFocus ></input></p>
-            <p><input type="submit" name="Create Post" ></input></p>
+            <p><input type="submit" name="Create Post" value="Create Post" ></input></p>
          </form>
       </>
    )
